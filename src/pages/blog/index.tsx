26668f0862f1abc920894345086bd15c4b9bc233
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2025-05-10 16:18:45 -0300

    feat: component active_link

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,12 @@
+import { Header } from "@/components/header";
+
+const BlogPage = () => {
+    return (
+        <>
+            <Header />
+            Blog
+        </>
+    );
+}
+
+export default BlogPage
