38eceb9023e7448a51312b38ad683144ed7a8afc
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2025-05-11 11:10:43 -0300

    feat: layout component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import { Header } from "@/components/header";
 import { Button } from "@/components/ui/button";
 import { Geist, Geist_Mono } from "next/font/google";
 
@@ -17,8 +16,6 @@ export default function Home() {
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
-      <Header />
-
       <Button variant={'secondary'}>Button</Button>
     </div>
   );
