eeccb45621c4dc431c8648e653a27182193c4e82
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2025-05-13 19:29:17 -0300

    refactor: structure

---
+++ src/templates/landing-page/sections/index.tsx
@@ -0,0 +1,5 @@
+export * from '../sections/hero-section';
+export * from '../sections/feature-section';
+export * from '../sections/support-section';
+export * from '../sections/customer-story-section';
+export * from '../sections/call-to-action';
